1a3bdb0eac71383b747b54bc32b9a0de29d575c7
BenFadhelOmar001 <110164705+BenFadhelOmar001@users.noreply.github.com>
2022-09-16 10:44:07 +0100

    Update __manifest__.py

--- emplois__fsegt/__manifest__.py
+++ emplois__fsegt/__manifest__.py
@@ -9,6 +9,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'price': '20.0'
     'category': 'Emplois',
     'version': '15.0.0.0.0',
 
